3a5a99e391477a6bb79e447dc45d2825646b57f8
Nafiril Haq Annibras <nafirilhaq.a@photon.in>
2021-03-19 19:33:16 +0700

    CT009 Home Page

--- src/components/checkout/ShippingMethod/ShippingMethod.js
+++ src/components/checkout/ShippingMethod/ShippingMethod.js
@@ -1,13 +1,13 @@
-import gql from 'graphql-tag';
-import { required } from 'vuelidate/lib/validators';
-import cartMixin from '../../../mixins/cartMixin';
-import BaseRadio from '../../common/form/BaseRadio/BaseRadio.vue';
-import BaseMoney from '../../common/BaseMoney/BaseMoney.vue';
-import BaseForm from '../../common/form/BaseForm/BaseForm.vue';
-import BaseLabel from '../../common/form/BaseLabel/BaseLabel.vue';
-import ServerError from '../../common/form/ServerError/ServerError.vue';
-import MONEY_FRAGMENT from '../../Money.gql';
-import { locale } from '../../common/shared';
+import gql from "graphql-tag";
+import { required } from "vuelidate/lib/validators";
+import cartMixin from "../../../mixins/cartMixin";
+import BaseRadio from "../../common/form/BaseRadio/BaseRadio.vue";
+import BaseMoney from "../../common/BaseMoney/BaseMoney.vue";
+import BaseForm from "../../common/form/BaseForm/BaseForm.vue";
+import BaseLabel from "../../common/form/BaseLabel/BaseLabel.vue";
+import ServerError from "../../common/form/ServerError/ServerError.vue";
+import MONEY_FRAGMENT from "../../Money.gql";
+import { locale } from "../../common/shared";
 
 export default {
   components: {
@@ -28,13 +28,14 @@ export default {
       return this.isFree(shippingRate) ? null : shippingRate.price;
     },
     matchingShippingRate(shippingMethod) {
-      return this.matchingZoneRate(shippingMethod).shippingRates
-        .find((shippingRate) => shippingRate.isMatching);
+      return this.matchingZoneRate(shippingMethod).shippingRates.find(
+        (shippingRate) => shippingRate.isMatching
+      );
     },
     matchingZoneRate(shippingMethod) {
-      return shippingMethod.zoneRates
-        .find((zoneRate) => zoneRate.shippingRates
-          .some((shippingRate) => shippingRate.isMatching));
+      return shippingMethod.zoneRates.find((zoneRate) =>
+        zoneRate.shippingRates.some((shippingRate) => shippingRate.isMatching)
+      );
     },
     isFree(shippingRate) {
       const totalPrice = this.me.activeCart.totalPrice.centAmount;
@@ -43,22 +44,25 @@ export default {
   },
   watch: {
     me(value) {
-      this.selectedShippingMethod = value?.activeCart?.shippingInfo?.shippingMethod?.id;
+      this.selectedShippingMethod =
+        value?.activeCart?.shippingInfo?.shippingMethod?.id;
     },
     shippingMethodsByLocation(value) {
       if (!this.selectedShippingMethod) {
-        this.selectedShippingMethod = value.find((shippingMethod) => shippingMethod.isDefault)?.id || value[0]?.id;
+        this.selectedShippingMethod =
+          value.find((shippingMethod) => shippingMethod.isDefault)?.id ||
+          value[0]?.id;
       }
     },
     selectedShippingMethod() {
-      if(!this.selectedShippingMethod){
-        return
+      if (!this.selectedShippingMethod) {
+        return;
       }
       this.updateMyCart([
         {
           setShippingMethod: {
             shippingMethod: {
-              typeId: 'shipping-method',
+              typeId: "shipping-method",
               id: this.selectedShippingMethod,
             },
           },
@@ -89,15 +93,25 @@ export default {
               }
             }
           }
-        }`,
+        }
+      `,
     },
     shippingMethodsByLocation: {
       query: gql`
-        query checkoutShippingMethods($currency: Currency!, $country: Country!, $state: String,$locale:Locale) {
-          shippingMethodsByLocation(currency: $currency, country: $country, state: $state) {
+        query checkoutShippingMethods(
+          $currency: Currency!
+          $country: Country!
+          $state: String
+          $locale: Locale
+        ) {
+          shippingMethodsByLocation(
+            currency: $currency
+            country: $country
+            state: $state
+          ) {
             id
             name
-            localizedDescription(locale:$locale)
+            localizedDescription(locale: $locale)
             isDefault
             zoneRates {
               shippingRates {
@@ -112,7 +126,8 @@ export default {
             }
           }
         }
-        ${MONEY_FRAGMENT}`,
+        ${MONEY_FRAGMENT}
+      `,
       variables() {
         return {
           currency: this.me.activeCart.totalPrice.currencyCode,
